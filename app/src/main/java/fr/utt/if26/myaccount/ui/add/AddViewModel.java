8707cf25564344fe80ebc7b31528a80d2fcaf2de
MaJingyiUtt <48433363+MaJingyiUtt@users.noreply.github.com>
2019-12-11 19:40:22 +0100

    Bottom navigation with fragments.

---
+++ app/src/main/java/fr/utt/if26/myaccount/ui/add/AddViewModel.java
@@ -0,0 +1,19 @@
+package fr.utt.if26.myaccount.ui.add;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AddViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AddViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/fr/utt/if26/myaccount/ui/chart/ChartFragment.java
@@ -0,0 +1,35 @@
+package fr.utt.if26.myaccount.ui.chart;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import fr.utt.if26.myaccount.R;
+
+public class ChartFragment extends Fragment {
+
+    private ChartViewModel chartViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        chartViewModel =
+                ViewModelProviders.of(this).get(ChartViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_chart, container, false);
+        final TextView textView = root.findViewById(R.id.text_notifications);
+        chartViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
